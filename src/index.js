c1360f70aec666a1f61f2359a244214009c195b7
bhupendra <upadhyaybhupendra24@gmail.com>
2024-02-23 03:17:17 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import connectDB from './db/index.js';
 const app = express(); 
 
 // Connect to MongoDB Atlas
-connectDB();
+connectDB()
 
-// Define routes and other middleware here
+.then(() => {
+  app.listen(process.env.PORT||3001, () =>{
+    console.log( `Server running on port ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGO db connection failed!!", err);
+})
 
-// Start the server
+
+
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
