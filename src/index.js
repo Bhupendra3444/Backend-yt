c742fd4bb61ca98ffab32fe276a577d76bd80a6d
bhupendra <upadhyaybhupendra24@gmail.com>
2024-02-24 22:55:43 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ const app = express();
 
 // Connect to MongoDB Atlas
 connectDB()
-
 .then(() => {
-  app.listen(process.env.PORT||3001, () =>{
-    console.log( `Server running on port ${process.env.PORT}`);
-  })
+    app.listen(process.env.PORT || 3001, () =>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
 })
-.catch((err) => {
-  console.log("MONGO db connection failed!!", err);
+.catch((err) =>{
+    console.log("MONGO db connection failed", err)
 })
 
+// Define routes and other middleware here
 
-
-
+// Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
